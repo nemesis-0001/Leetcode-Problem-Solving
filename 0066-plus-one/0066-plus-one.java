365ca4207f0ed11eceb3ed8647e16a2aca9db85c
nemesis-0001 <126962978+nemesis-0001@users.noreply.github.com>
2024-06-28 21:42:23 +0530

    Time: 0 ms (100%), Space: 41.5 MB (70.19%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -2,8 +2,7 @@ class Solution {
     public int[] plusOne(int[] digits) {
 
          int n = digits.length;
-//        int[] res = new int[]{digits.length};
-        // digits[n]+=1;
+         
         for (int i= n-1;i>=0;i--){
             if (digits[i]<9){
                 digits[i]++;
