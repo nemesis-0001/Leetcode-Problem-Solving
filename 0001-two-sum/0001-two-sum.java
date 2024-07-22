89a5375fe6c81d37bc79e1806187c499882f9c90
nemesis-0001 <126962978+nemesis-0001@users.noreply.github.com>
2024-07-22 22:05:47 +0530

    Time: 2 ms (97.93%), Space: 44.6 MB (68.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,28 +1,15 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for(int i=0;i<nums.length;i++){
-            int res;
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j] == target){
-                    return new int[]{i,j};
+                      Map<Integer,Integer> comps = new HashMap<>();
+
+            for(int i=0;i<nums.length;i++){
+                Integer comp = comps.get(nums[i]);
+                if(comp != null){
+                    return new int[] {i,comp};
                 }
+                comps.put(target-nums[i],i);
             }
-        }
-        return new int[]{-1,-1};
-    }
 
-    public static void main(String[] args){
-        
-        int[] nums={1,2,3,4};
-        
-        int target=7;
-
-        Solution sol = new Solution();
-
-        int[] result =  sol.twoSum(nums,target);
-
-        if(result[0]!=-1 && result[1]!=-1){
-            System.out.println("["+result[0]+","+result[1]+"]");
-        }
+            return new int[]{};
     }
 }
