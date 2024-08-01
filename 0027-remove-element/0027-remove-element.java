050130b5e179686fe020abd8e88df4d482a84035
nemesis-0001 <126962978+nemesis-0001@users.noreply.github.com>
2024-08-01 17:25:24 +0530

    Time: 0 ms (100%), Space: 41.8 MB (59.78%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int i=0;
+        int k=0;
 
-        for(int j=0;j<nums.length;j++){
-            if (nums[j] != val) {
-                nums[i]=nums[j];
-                i++;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
             }
         }
 
-        return i;
+        return k;
     }
 }
