bc7a9da2b0f52f7aef93feff42da0326d2185aec
nemesis-0001 <126962978+nemesis-0001@users.noreply.github.com>
2024-09-12 10:24:17 +0530

    Time: 0 ms (100%), Space: 44.5 MB (91.58%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,14 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int i=0;
-
+        // int dup=nums[i];
         for(int j=1;j<nums.length;j++){
+            // for()
             if(nums[i]!=nums[j]){
                 i++;
                 nums[i]=nums[j];
             }
         }
-
         return i+1;
     }
 }
